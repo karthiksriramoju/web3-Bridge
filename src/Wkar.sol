763cc9115bafbc9bc20b9c4811c298d503106f77
Your Name <karthiksriramoju11@gmail.com>
2025-02-01 18:34:36 +0530

    eroors

--- src/Wkar.sol
+++ src/Wkar.sol
@@ -5,7 +5,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Wkar is ERC20, Ownable{
-  constructor() ERC20("WKARTHIK", "Wkar"){}
+  constructor() ERC20("WKARTHIK", "Wkar") Ownable(msg.sender){}
 
   function mint(address _to,uint256 _value) public onlyOwner {
     _mint(_to, _value);
